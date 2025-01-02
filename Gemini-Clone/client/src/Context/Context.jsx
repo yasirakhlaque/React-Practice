2192382ff8d8e02b4b0449e21aaacc287b59ed7a
Yasir Akhlaque <akhlaqueyasir28@gmail.com>
2025-01-02 14:58:57 +0530

    added link

--- Gemini-Clone/client/src/Context/Context.jsx
+++ Gemini-Clone/client/src/Context/Context.jsx
@@ -24,7 +24,7 @@ const ContxtProvider = (props) => {
         setLoading(true);
 
         try {
-            const res = await fetch('http://localhost:5000/api/generate', {
+            const res = await fetch('https://clone-two-blush.vercel.app/api/generate', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
